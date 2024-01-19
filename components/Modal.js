9f4d10641be74cc5dd93ec78cd949390914fb213
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2024-01-19 21:47:10 +0530

    Update Modal.js

--- components/Modal.js
+++ components/Modal.js
@@ -5,13 +5,19 @@ const Modal = ({ children, isOpen, onClose }) => {
   if (!isOpen) return null;
 
  //add code here
-    <div className='modal'>
+
+    return ReactDOM.createPortal(
+          <div className='modal'>
       
       <div style={{ backgroundColor: 'white', padding: '2rem', position: 'relative' }} onClick={(e) => e.stopPropagation()} >// This prevents the modal from closing when clicking inside
    //add code here
+        <button onClick={onClose}>Close Modal </button>
+          {children}
       </div>
     </div>,
     document.body
+
+);
 
 };
 
