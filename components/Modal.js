9669f3445e0e36cbfeb741d34a7d56412bb0bf98
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2024-01-19 22:08:50 +0530

    Update Modal.js

--- components/Modal.js
+++ components/Modal.js
@@ -5,16 +5,28 @@ const Modal = ({ children, isOpen, onClose }) => {
   if (!isOpen) return null;
 
  //add code here
+  const handleOverlayClose = (e) => {
+
+    //check if the click was on the overlay (outside the modal)
+    if(e.target === e.currentTarget){
+      onClose();
+    }
+    
+      
+  }
 
     return ReactDOM.createPortal(
-          <div className='modal'>
+          <div className='modal-overlay'  onClick={handleOverlayClose}>
+          <div className='modal' >
       
       <div style={{ backgroundColor: 'white', padding: '2rem', position: 'relative' }} onClick={(e) => e.stopPropagation()} >// This prevents the modal from closing when clicking inside
    //add code here
         <button onClick={onClose}>Close Modal </button>
           {children}
       </div>
-    </div>,
+    </div>
+      </div>
+      ,
     document.body
 
 );
